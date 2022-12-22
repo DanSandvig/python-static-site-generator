bafa9f721e26e79f39e3a2fb2f76c9f26eaa7713
DanSandvig <dspublicaddress@protonmail.com>
2022-12-22 11:52:34 +0000

    First attempt at section 3

--- ssg/site.py
+++ ssg/site.py
@@ -3,17 +3,36 @@ from pathlib import Path
 class Site:
     """Definition of a basic website structure"""
     
-    def __init__(self, source, dest) -> None:
+    def __init__(self, source, dest, parsers=None) -> None:
         self.source = Path(source)
         self.dest = Path(dest)
-        
-    def create_dir(self, path):
+        self.parsers = parsers or []
+
+
+    def create_dir(self, path: Path):
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
-        
+
+
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
         
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
+
+
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
+
+
+    def run_parser(self, path: Path):
+        parser = self.load_parser(path.suffix)
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            print("Not Implemented")
