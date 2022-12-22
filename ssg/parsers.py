37c4548bdd28a305adcc78ae2417fc7197883ffc
DanSandvig <dspublicaddress@protonmail.com>
2022-12-22 12:03:32 +0000

    Try adding 'r' mode parameter to 'with open' statement

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -15,14 +15,14 @@ class Parser:
     
     
     def read(self, path: Path):
-        with open(path) as file:
+        with open(path, "r") as file:
             return file.read()
         
         
     def write(self, path: Path, dest: Path, content, ext=".html"):
         full_path = dest / path.with_suffix(ext).name
         
-        with open(full_path, mode='w') as file:
+        with open(full_path, "w") as file:
             file.write(content)
 
 
