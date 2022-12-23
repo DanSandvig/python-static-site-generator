f56cac816e78163bf8b5367a368d2dcf47db98ed
DanSandvig <dspublicaddress@protonmail.com>
2022-12-23 09:12:39 +0000

    First pass at final section

--- ssg/site.py
+++ ssg/site.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import sys
 
 class Site:
     """Definition of a basic website structure"""
@@ -35,4 +36,9 @@ class Site:
         if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
-            print("Not Implemented")
+            self.error("No parser for the {} extension, file skipped!".format())
+
+
+    @staticmethod
+    def error(message):
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
